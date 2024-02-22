78269e08a9c37a0bd3c19fdbfff1acb47b160821
butko denis <butko.dv@gmail.com>
2024-02-22 19:45:04 +0000

    Update server configuration

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const morgan = require('morgan');
+const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-require('dotenv').config();
-
-
-
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 const { MONGO_URL, NODE_PORT } = process.env;
 
 mongoose
@@ -18,16 +13,10 @@ mongoose
   .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
 
-  
 
 app.use(morgan('tiny'));
 app.use(cors());
-
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from server' });
-});
-
-
+app.use(express.json());
 
 const port = NODE_PORT || 4001;
 app.listen(port, () => {
