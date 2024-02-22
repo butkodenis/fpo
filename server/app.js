04a0d241ee2a95c5faad0027ddee202f3462aecd
butko denis <butko.dv@gmail.com>
2024-02-22 10:11:37 +0000

    Update server configuration and Docker setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-
 const app = express();
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+
+
+const { MONGO_URL, NODE_PORT } = process.env;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+  
 
 app.use(morgan('tiny'));
 app.use(cors());
@@ -12,7 +27,9 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from server' });
 });
 
-const port = process.env.NODE_PORT || 4000;
+
+
+const port = NODE_PORT || 4001;
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
