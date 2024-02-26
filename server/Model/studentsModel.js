359b22f113062bd8f45abd26433c4461738a12d2
butko denis <butko.dv@gmail.com>
2024-02-26 13:51:53 +0000

    Update Prettier configuration and add prettier-plugin-tailwindcss

--- server/Model/studentsModel.js
+++ server/Model/studentsModel.js
@@ -1,50 +1,29 @@
 const mongoose = require('mongoose');
 
 const cardStudentsSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  middleName: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  passport: {
-    id: { String, required: true },
-  },
-  address: {
-    city: { String, required: true },
-    street: { String, required: true },
-    house: { String, required: true },
-    flat: { String, required: true },
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  middleName: { type: String, required: true },
+  info: {
+    age: { type: Number, required: true },
+    passport_id: { type: String },
+    address: {
+      city: { String, required: true },
+      street: { String, required: true },
+      house: { String, required: true },
+      flat: { String, required: true },
+    },
+    phone: { type: String, required: true },
+    email: { type: String },
   },
   course: [
     {
-      courseName: { type: String, required: true },
-      courseStartDate: { type: Date, required: true },
-      courseEndDate: { type: Date, required: true },
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      activ: { type: Boolean, default: false },
     },
   ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('CardStudents', cardStudentsSchema);
