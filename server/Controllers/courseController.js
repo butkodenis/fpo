caad663be24792968bfe1f1fe5fc3d8e47aba10b
butko denis <butko.dv@gmail.com>
2024-02-27 21:55:51 +0000

    Add updateCourse and deleteCourse functions to courseController.js

--- server/Controllers/courseController.js
+++ server/Controllers/courseController.js
@@ -34,4 +34,33 @@ const createCourse = async (req, res) => {
   }
 };
 
-module.exports = { getAllCourses, createCourse };
+const updateCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { typeCourse, specialty, duration, price, order } = req.body;
+    const existingCourse = await Course.findOne({ _id: id });
+    if (!existingCourse) {
+      return res.status(400).json({ message: 'Курс не знайдено' });
+    }
+    await Course.findByIdAndUpdate(id, { typeCourse, specialty, duration, price, order });
+    res.status(200).json({ message: 'Курс оновлено' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const existingCourse = await Course.findOne({ _id: id });
+    if (!existingCourse) {
+      return res.status(400).json({ message: 'Курс не знайдено' });
+    }
+    await Course.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Курс видалено' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllCourses, createCourse, updateCourse, deleteCourse };
