fba522786bff535b2d749c075559fe86e81ede2b
butko denis <butko.dv@gmail.com>
2024-02-29 21:46:24 +0000

    Update course field in studentModel.js

--- server/Model/studentModel.js
+++ server/Model/studentModel.js
@@ -6,9 +6,11 @@ const studentSchema = new mongoose.Schema({
   middleName: { type: String, required: true },
   phone: { type: String },
 
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+  course: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
 
   createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Student', studentSchema);
+{
+}
