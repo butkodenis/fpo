5af568af6303a791cf95231f3c75f1f766f96de4
butko denis <butko.dv@gmail.com>
2024-03-04 20:26:43 +0000

    Add Bootstrap CSS and JS files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1>React App</h1>
+            <img src={reactLogo} alt="react-logo" width="200" />
+          </div>
+          <div className="col-md-6">
+            <h1>Vite App</h1>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
