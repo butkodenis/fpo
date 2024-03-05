1f7e8164259c8b4d6a606bbdd5347600fa7bb73f
butko denis <butko.dv@gmail.com>
2024-03-05 21:45:59 +0000

    Update font and add new login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/students" element={<Students />} />
           </Route>
+          <Route path="/login2" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
