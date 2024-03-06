601bc4c526141f5634226bd2e41e08e184d6501d
butko denis <butko.dv@gmail.com>
2024-03-06 21:23:39 +0000

    Refactor Students component to display student information

--- client/src/components/pages/Students.jsx
+++ client/src/components/pages/Students.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Students = () => {
   return (
-    <div className=" ">
+    <div className="d-flex flex-column   ">
       <h1>Students</h1>
+      <div className="d-flex flex-column">
+        <div className="d-flex">
+          <div className="d-flex flex-column">
+            <h2>Student 1</h2>
+            <p>Student 1 info</p>
+          </div>
+          <div className="d-flex flex-column">
+            <h2>Student 2</h2>
+            <p>Student 2 info</p>
+          </div>
+        </div>
+        <div className="d-flex">
+          <div className="d-flex flex-column">
+            <h2>Student 3</h2>
+            <p>Student 3 info</p>
+          </div>
+          <div className="d-flex flex-column">
+            <h2>Student 4</h2>
+            <p>Student 4 info</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
