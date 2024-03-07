30a1ff1109acbda1115181166ff4f114c2156214
butko denis <butko.dv@gmail.com>
2024-03-07 16:51:07 +0000

    Add table to display student information

--- client/src/components/pages/Students.jsx
+++ client/src/components/pages/Students.jsx
@@ -1,34 +1,91 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
 
 const Students = () => {
   const [students, setStudents] = useState([]);
 
+  const fetchStudents = async () => {
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/student/getAll`);
+      setStudents(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
   return (
-    <div className="d-flex flex-column   ">
+    <div className="d-flex flex-column">
       <h1>Students</h1>
-      <div className="d-flex flex-column">
-        <div className="d-flex">
-          <div className="d-flex flex-column">
-            <h2>Student 1</h2>
-            <p>Student 1 info</p>
-          </div>
-          <div className="d-flex flex-column">
-            <h2>Student 2</h2>
-            <p>Student 2 info</p>
-          </div>
-        </div>
-        <div className="d-flex">
-          <div className="d-flex flex-column">
-            <h2>Student 3</h2>
-            <p>Student 3 info</p>
-          </div>
-          <div className="d-flex flex-column">
-            <h2>Student 4</h2>
-            <p>Student 4 info</p>
-          </div>
-        </div>
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Middle Name</th>
+            <th>Phone</th>
+            <th>Старт курсів</th>
+            <th>Кінець курсів</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr key={student._id}>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.middleName}</td>
+              <td>{student.phone}</td>
+              <td>
+                {student.courses.length > 0 &&
+                  new Date(student.courses[student.courses.length - 1].startDate)
+                    .toISOString()
+                    .split('T')[0]}
+              </td>
+              <td>
+                {student.courses.length > 0 &&
+                  new Date(student.courses[student.courses.length - 1].endDate)
+                    .toISOString()
+                    .split('T')[0]}
+              </td>
+
+              <td>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Action
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Додати курс
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Оновити дінні
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item " href="#">
+                        Видалити
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
