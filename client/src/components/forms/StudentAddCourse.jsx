c98e7a40518fe453ca950539f3e0f4e3a5e7d8d9
butko denis <butko.dv@gmail.com>
2024-03-12 15:05:57 +0000

    Add link to navigate back in StudentAddCourse form

--- client/src/components/forms/StudentAddCourse.jsx
+++ client/src/components/forms/StudentAddCourse.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
@@ -12,7 +12,6 @@ const StudentAddCourse = () => {
     try {
       const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/course/getAll`);
       setCourses(res.data);
-      console.log(res.data);
     } catch (error) {
       console.error(error);
     }
@@ -20,13 +19,12 @@ const StudentAddCourse = () => {
 
   const onSubmit = async (data) => {
     console.log(data);
-    /*
+
     try {
       await axios.post(`${import.meta.env.VITE_BASE_URL}/api/student/${id}/addCourse`, data);
     } catch (error) {
       console.error(error);
     }
-    */
   };
 
   useEffect(() => {
@@ -39,10 +37,10 @@ const StudentAddCourse = () => {
           <h3>Додати курс</h3>
           <p>курс : {id}</p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-50 mb-3 ">
           <div className="form-group mb-3">
             <label htmlFor="course">Курс</label>
-            <select className="form-control" id="course" {...register('course')}>
+            <select className="form-control" id="course" {...register('courseId')}>
               <option>Виберіть курс</option>
               {courses.map((course) => (
                 <option key={course._id} value={course._id}>
@@ -55,13 +53,16 @@ const StudentAddCourse = () => {
             <label htmlFor="startDate">Дата початку</label>
             <input type="date" className="form-control" id="startDate" {...register('startDate')} />
           </div>
-          <div className="form-group mb-3">
+          <div className="form-group mb-3 ">
             <label htmlFor="endDate">Дата закінчення</label>
             <input type="date" className="form-control" id="endDate" {...register('endDate')} />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary ">
             Додати
           </button>
+          <Link to="/students" className="btn btn-primary ">
+            Повернутися
+          </Link>
         </form>
       </div>
     </>
