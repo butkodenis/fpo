c4deb605ad5f867765e6b20a794d201e0d38f2f0
butko denis <butko.dv@gmail.com>
2024-03-20 21:08:57 +0000

    Add form functionality to enable editing contract dates

--- client/src/components/forms/FormContract.jsx
+++ client/src/components/forms/FormContract.jsx
@@ -2,27 +2,53 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const FormContract = ({ contract }) => {
+  const [activeForm, setActiveForm] = useState(false);
   const { register, handleSubmit } = useForm({
     defaultValues: {
       startDate: contract.startDate.split('T')[0],
       endDate: contract.endDate.split('T')[0],
     },
   });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setActiveForm(!activeForm);
+  };
+
   return (
     <div className="mb-3">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="row">
           <div className="form-group col-6">
             <label htmlFor="startDate">Дата початку</label>
-            <input type="date" className="form-control" id="startDate" {...register('startDate')} />
+            <input
+              type="date"
+              className="form-control"
+              id="startDate"
+              {...register('startDate')}
+              disabled={!activeForm}
+            />
           </div>
           <div className="form-group col-6">
             <label htmlFor="endDate">Дата закінчення</label>
-            <input type="date" className="form-control" id="endDate" {...register('endDate')} />
+            <input
+              type="date"
+              className="form-control"
+              id="endDate"
+              {...register('endDate')}
+              disabled={!activeForm}
+            />
           </div>
         </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => setActiveForm(!activeForm)}
+        >
+          <i className="bi bi-pencil-fill"></i>
+        </button>
         <button type="submit" className="btn btn-primary">
-          Зберегти
+          <i className="bi bi-floppy"></i>
         </button>
       </form>
     </div>
