b88fc5f36d9635d601ffef6c38b8f5b7cac1ca01
butko denis <butko.dv@gmail.com>
2024-03-28 22:16:29 +0000

    Add closeMonth function to studentBalanceController.js

--- server/Controllers/studentBalanceController.js
+++ server/Controllers/studentBalanceController.js
@@ -10,4 +10,43 @@ const getAllStudentsBalance = async (req, res) => {
   }
 };
 
-module.exports = { getAllStudentsBalance };
+const closeMonth = async (req, res) => {
+  try {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+
+    const nextMonth = month === 12 ? 1 : month + 1;
+    const nextYear = month === 12 ? year + 1 : year;
+
+    const studentsBalance = await StudentsBalance.find({
+      year,
+      month,
+      balanceEnd: { $ne: 0 },
+    }).populate('student');
+    // console.log(studentsBalance);
+
+    const studentsBalanceNextMonth = studentsBalance.map((studentBalance) => {
+      const { student, balanceEnd } = studentBalance;
+      return {
+        student,
+        balanceStart: balanceEnd,
+        balanceEnd: 0,
+        accrued: 0,
+        payment: 0,
+        year: nextYear,
+        month: nextMonth,
+      };
+    });
+
+    // console.log(studentsBalanceNextMonth);
+
+    await StudentsBalance.insertMany(studentsBalanceNextMonth);
+
+    res.status(200).json({ message: 'Місяць закрито' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllStudentsBalance, closeMonth };
