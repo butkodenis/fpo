04fd38c5220ec749b1907c757d9827413fa9b3f2
butko denis <butko.dv@gmail.com>
2024-04-18 12:45:09 +0000

    Refactor component imports and update styling in Payment.jsx, Students.jsx, and PaymentTable.jsx

--- client/src/components/forms/StudentAddContract.jsx
+++ client/src/components/forms/StudentAddContract.jsx
@@ -3,6 +3,15 @@ import { Link, useParams, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
 const StudentAddContract = () => {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -20,6 +29,8 @@ const StudentAddContract = () => {
 
   const onSubmit = async (data) => {
     try {
+      console.log(data);
+
       const res = await axios.post(
         `${import.meta.env.VITE_BASE_URL}/api/student/${id}/addContract`,
         data,
@@ -39,99 +50,103 @@ const StudentAddContract = () => {
 
   return (
     <>
-      <div className="d-flex ">
+      <Box sx={{ width: 720 }}>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group mb-3">
-            <label htmlFor="course">Курс</label>
-            <select className="form-control" id="course" {...register('courseId')}>
-              <option>Виберіть курс</option>
-              {courses.map((course) => (
-                <option key={course._id} value={course._id}>
-                  {course.typeCourse} {course.specialty} {course.price}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="row">
-            <div className="form-group col-4 mb-3">
-              <label htmlFor="startDate">Дата початку</label>
-              <input
-                type="date"
-                className="form-control"
-                id="startDate"
-                {...register('startDate')}
-              />
-            </div>
-
-            <div className="form-group col-4 mb-3 ">
-              <label htmlFor="endDate">Дата закінчення</label>
-              <input type="date" className="form-control" id="endDate" {...register('endDate')} />
-            </div>
-
-            <div className="form-group col-4 mb-3">
-              <label htmlFor="numOrder">№ наказу</label>
-              <input
-                type="number"
-                className="form-control"
-                id="numOrder"
-                {...register('numOrder')}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="form-group col-8 mb-3">
-              <label htmlFor="urFullName">Юр. особа</label>
-              <input
-                type="text"
-                className="form-control"
-                id="urFullName"
-                {...register('urFullName')}
-              />
-            </div>
-            <div className="form-group col-4 mb-3">
-              <label htmlFor="edrpou">ЄДРПОУ</label>
-              <input type="text" className="form-control" id="edrpou" {...register('edrpou')} />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="form-group col-3 mb-3">
-              <label htmlFor="num">Номер договору</label>
-              <input type="text" className="form-control" id="num" {...register('num')} />
-            </div>
-            <div className="form-group col-3 mb-3 ">
-              <label htmlFor="numDate">Від</label>
-              <input type="date" className="form-control" id="numDate" {...register('numDate')} />
-            </div>
-            <div className="form-group col-3 mb-3">
-              <label htmlFor="part">Частинами</label>
-              <input
-                type="number"
-                className="form-control"
-                id="sum"
-                min="1"
-                max="8"
-                defaultValue="1"
-                {...register('part')}
-              />
-            </div>
-
-            <div className="form-group col-3 mb-3 ">
-              <label htmlFor="payDate">Дата оплати</label>
-              <input type="date" className="form-control" id="payDate" {...register('payDate')} />
-            </div>
-          </div>
-
-          <div className="d-flex justify-content-between">
-            <button type="submit" className="btn btn-info ">
-              Додати
-            </button>
-            <Link to="/students" className="btn btn-info ">
-              Повернутися
-            </Link>
-          </div>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                label="Курс"
+                select
+                id="courses"
+                fullWidth
+                helperText="Выберите курс"
+                {...register('courseId')}
+              >
+                {courses.map((course) => (
+                  <MenuItem key={course._id} value={course._id}>
+                    {`${course.typeCourse} ${course.specialty} (${course.price})`}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item container spacing={2} xs={12}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Дата начала"
+                  type="date"
+                  {...register('startDate')}
+                  fullWidth
+                  defaultValue={new Date().toISOString().slice(0, 10)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Дата окончания"
+                  type="date"
+                  {...register('endDate')}
+                  fullWidth
+                  defaultValue={new Date().toISOString().slice(0, 10)}
+                />
+              </Grid>
+            </Grid>
+            <Grid item container spacing={2} xs={12}>
+              <Grid item xs={12} sm={8}>
+                <TextField label="Юр. особа" {...register('urFullName')} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField label="ЄДРПОУ" {...register('edrpou')} fullWidth />
+              </Grid>
+            </Grid>
+            <Grid item container spacing={2} xs={12}>
+              <Grid item xs={12} sm={3}>
+                <TextField label="№ договору" type="number" {...register('num')} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  label="Від"
+                  type="date"
+                  defaultValue={new Date().toISOString().slice(0, 10)}
+                  {...register('numDate')}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  label="Частинами"
+                  type="number"
+                  {...register('part')}
+                  defaultValue={1}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  label="Дата оплати"
+                  type="date"
+                  {...register('payDate')}
+                  fullWidth
+                  defaultValue={new Date().toISOString().slice(0, 10)}
+                  helperText="Дата оплаты"
+                />
+              </Grid>
+            </Grid>
+            <Grid item container spacing={2} xs={12}>
+              <Grid item xs={12} sm={3}>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
+                  Додати
+                </Button>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <Link to="/students">
+                  <Button variant="contained" color="primary">
+                    Повернутися
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
         </form>
-      </div>
+      </Box>
     </>
   );
 };
