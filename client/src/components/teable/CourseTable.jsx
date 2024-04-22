8d7672466469ebb0aa9d7da73718623f20ced3bf
butko denis <butko.dv@gmail.com>
2024-04-22 20:28:50 +0000

    Refactor CourseTable component imports and update column headers

--- client/src/components/teable/CourseTable.jsx
+++ client/src/components/teable/CourseTable.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { useTable } from 'react-table';
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 import axios from 'axios';
-import { formatDate } from '../util';
 
 const CourseTable = () => {
   const [courseData, setCourseData] = useState([]);
@@ -23,66 +22,39 @@ const CourseTable = () => {
   const columns = useMemo(
     () => [
       {
-        Header: 'Тип курсу',
-        accessor: 'specialty',
+        header: 'Тип курсу',
+        accessorKey: 'specialty',
       },
       {
-        Header: 'Спеціальність/Профіль',
-        accessor: 'typeCourse',
+        header: 'Спеціальність/Профіль',
+        accessorKey: 'typeCourse',
       },
       {
-        Header: 'Тривалість навчання	',
-        accessor: 'duration',
+        header: 'Тривалість навчання	',
+        accessorKey: 'duration',
       },
       {
-        Header: 'Вартість навчання, грн',
-        accessor: 'price',
+        header: 'Вартість навчання, грн',
+        accessorKey: 'price',
       },
       {
-        Header: 'Наказ №',
-        accessor: 'order',
+        header: 'Наказ №',
+        accessorKey: 'order',
       },
       {
-        Header: 'Дата наказу',
-        accessor: 'orderDate',
-        Cell: ({ value }) => formatDate(value),
+        header: 'Дата наказу',
+        accessorKey: 'orderDate',
       },
     ],
     [],
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+  const table = useMaterialReactTable({
     columns,
     data: courseData,
   });
 
-  return (
-    <div className="conteiner rounded ">
-      <table className="table-sm  table-striped table border" {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+  return <MaterialReactTable table={table} />;
 };
 
 export default CourseTable;
